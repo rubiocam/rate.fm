6a2090dfd4223543bd92da3728849811f5e23811
Samantha Rubio-Campos <sam_rubioc05@yahoo.com>
2026-05-30 23:11:22 -0700

    backend: initial intelliJ project

---
+++ backend/rate_fm/src/main/java/com/rubiocam/rate_fm/RateFmApplication.java
@@ -0,0 +1,13 @@
+package com.rubiocam.rate_fm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateFmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateFmApplication.class, args);
+    }
+
+}
---
+++ backend/rate_fm/src/test/java/com/rubiocam/rate_fm/RateFmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubiocam.rate_fm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateFmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
